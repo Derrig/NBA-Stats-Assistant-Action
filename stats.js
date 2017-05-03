3c1ae9d72ce383b56b357cde96b9f7dadff9f41b
Derrig <derrig@live.com>
2017-05-03 15:33:30 -0400

    TS & PPG

--- stats.js
+++ stats.js
@@ -1,8 +1,23 @@
 const nba = require('nba.js').default;
 
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
 function ppg(player){
-    console.log(player['resultSets'][0]['rowSet'][26])
+    var regSeason = player['SeasonTotalsRegularSeason']
+    console.log(regSeason[regSeason.length-1]['pts'])
+}
+
+function ts(player){
+    var regSeason = player['SeasonTotalsRegularSeason']
+    var curSeason = regSeason[regSeason.length-1]
+    var fga = curSeason['fga']
+    var fta = curSeason['fta']
+    var pts = curSeason['pts']
+    var ts = pts/(2*(fga+.44*fta))
+    console.log(round(ts,3))
 }
 
 nba.stats.playerCareerStats({PerMode:'PerGame',PlayerID:'2546'})
-                                .then(res => {ppg(res)});
+                                .then(ts)
